54e21dbe1770ce169b04c1995a4bb525a493e5d5
Andre Pierre <andre.laurence94@gmail.com>
2021-06-16 02:40:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  while number > 10 
+  while number < 10 
   puts "#{number} SECOND(S)!"
-  countdown_output =- 1 
+  countdown_output -= 1 
   puts "HAPPY NEW YEAR!"
   end
 end
