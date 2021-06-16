2df20e9fccc8ffb5d5c69b4c333663d7d16dd97f
Andre Pierre <andre.laurence94@gmail.com>
2021-06-16 02:45:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(number)
   while number > 11
   print "#{number} SECOND(S)!"
   countdown_output -= 1 
-  unless number == 1 
+  until number == 1 
   return "HAPPY NEW YEAR!"
   end
 end
